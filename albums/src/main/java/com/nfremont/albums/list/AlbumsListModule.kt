c03e6f2fb176c5795d68228eafa3101289fccdea
Nathan Frémont <nathan.fremontg@gmail.com>
2022-02-26 19:45:08 +0100

    [Step 3, use logic into view] Add ViewModel in DI

--- albums/src/main/java/com/nfremont/albums/list/AlbumsListModule.kt
+++ albums/src/main/java/com/nfremont/albums/list/AlbumsListModule.kt
@@ -2,9 +2,11 @@ package com.nfremont.albums.list
 
 import com.nfremont.albums.list.datasource.AlbumsListDataSource
 import com.nfremont.albums.list.datasource.AlbumsListDataSourceImpl
+import com.nfremont.albums.list.infrastructure.AlbumsListViewModel
 import com.nfremont.albums.list.repository.AlbumsListRepository
 import com.nfremont.albums.list.repository.AlbumsListRepositoryImpl
 import com.nfremont.core.CacheManager
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val albumsListModule = module {
@@ -13,5 +15,8 @@ val albumsListModule = module {
     }
     single<AlbumsListRepository> {
         AlbumsListRepositoryImpl(dataSource = get(), cacheManager = CacheManager())
+    }
+    viewModel {
+        AlbumsListViewModel(albumsListRepository = get())
     }
 }
